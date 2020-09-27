b49bd93d8833726cb9f814e7f67746414390c274
cocomuffinzz <crollins@uwm.edu>
2020-09-27 15:13:50 -0500

    created player health/mana

---
+++ Assets/Scripts/PlayerManaBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManaBehavior : MonoBehaviour
+{
+    private Text thisText;
+    private int mana;
+    
+    void Start()
+    {
+        thisText = GetComponent<Text>();
+        
+        // set mana initially to be 1
+        mana = 1;
+    }
+    
+    void Update() 
+    {
+        // // When Player's turn ends
+        // if(player turn ends)
+        // {
+        //     // add 1 to mana
+        //     mana += 1;
+        // }
+        
+        // update text of PlayerMana element
+        thisText.text = "Mana: " + mana;
+    }
+
+}
