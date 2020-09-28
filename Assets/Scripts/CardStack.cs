e506acf321af3f8bc0cbded794f5042ccb5ce581
zkennow <zachkennow@gmail.com>
2020-09-27 22:14:01 -0500

    Added basic functionallity to the card UI
    Added scripts for card, card stacks (deck, graveyard, player hand, and playing field), playing field, and player hand
    
    Added functionality to keep track of cards of a stack in a list
    
    Added drag and drop functionality
    
    Added card-flip effect when drawing a card from deck to hand
    
    Changed the test scene so there are no cards in the graveyard, player hand, or player field when starting the game (Since there is now drag and drop functionality)

---
+++ Assets/Scripts/CardStack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardStack : MonoBehaviour
+{
+    List<Card> cards = new List<Card>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //For every child in the card stack, add it to the card list. (This is entirely for the deck)
+        for (int i = 0; i <  this.gameObject.transform.childCount; i++)
+        {
+            Card card = this.gameObject.transform.GetChild(i).GetComponent<Card>();
+            cards.Add(card);
+        }
+        Debug.Log(this.GetSize());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetSize()
+    {
+        return cards.Count;
+    }
+
+    public void AddCard(Card card)
+    {
+        cards.Add(card);
+    }
+
+    public void DrawCard(Card card)
+    {
+        cards.Remove(card); //Removes the drawn card from the card list
+    }
+
+    //Shuffles the order of the cards
+    public void Shuffle()
+    {
+        //TODO
+    }
+}
---
+++ Assets/Scripts/PlayerHand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerHand : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    //Pointer enter and exit are required for OnDrop to work
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //TODO
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //TODO
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+        Card card = eventData.pointerDrag.GetComponent<Card>(); //Gets the card that was dropped
+        if (card != null)
+        {
+            card.SetCurrParent(this.transform); //Sets the card's current parent to the player hand
+            card.FlipCard(); //Flips the card over
+        }
+    }
+}
