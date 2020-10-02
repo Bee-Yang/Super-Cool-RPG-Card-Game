1fb5037d37671230ffd20984830bc6b4b9edb49f
cocomuffinzz <crollins@uwm.edu>
2020-10-02 02:14:10 -0500

    Added UI components

---
+++ Assets/Scripts/EnemyGraveyardCountBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyGraveyardCountBehavior : MonoBehaviour
+{
+    private Text thisText;
+    private int count;
+
+    void Start()
+    {
+        thisText = GetComponent<Text>();
+        
+        // set count initially to be 0
+        count = 0;
+    }
+
+    void Update()
+    {
+        // // When a card goes into graveyard
+        // if(graveyard size increases)
+        // {
+        //     // add 1 to count
+        //     count += 1;
+        // }
+        
+        // update text of EnemyGraveyardCount element
+        thisText.text = "" + count;
+    }
+}
---
+++ Assets/Scripts/EnemyManaBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyManaBehavior : MonoBehaviour
+{
+    private Text thisText;
+    private int mana;
+    
+    void Start()
+    {
+        thisText = GetComponent<Text>();
+        
+        // set mana initially to be 1
+        mana = 1;
+    }
+    
+    void Update() 
+    {
+        // // When Enemy's turn ends
+        // if(enemy turn ends)
+        // {
+        //     // add 1 to mana
+        //     mana += 1;
+        // }
+        
+        // update text of EnemyMana element
+        thisText.text = "Mana: " + mana;
+    }
+}
