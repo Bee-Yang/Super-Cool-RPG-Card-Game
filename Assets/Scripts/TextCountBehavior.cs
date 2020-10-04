57737071da5a6cc42652e086ddc39032013a7b75
Bee-Yang <yang737@uwm.edu>
2020-10-04 05:54:54 -0500

    Changed Text to TextMeshPro
    -Changed all textfields to textmeshpro
    -Changed text color of card cost and card health
    -Created generic script for changing text field of count for deck and graveyard

---
+++ Assets/Scripts/TextCountBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextCountBehavior : MonoBehaviour
+{
+    public GameObject obj;
+    private TMP_Text thisText;
+    private int count;
+
+    void Start()
+    {
+        thisText = GetComponent<TMP_Text>();
+        
+        // set count initially to be 0
+        count = 0;
+    }
+
+    void Update()
+    {
+        // When a card goes into graveyard
+        if(obj.transform.childCount != count)
+        {
+             // change the count
+             count = obj.transform.childCount;
+        }
+        
+        // update text of PlayerGraveyardCount element
+        thisText.text = "" + count;
+    }
+}
