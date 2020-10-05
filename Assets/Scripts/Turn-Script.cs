9f4de829dbf88efed090942b2624ce6edb4b2c84
JAG0824 <jahan@uwm.edu>
2020-10-04 21:58:47 -0500

    Turn/EndTuRn/TurnScript Code for use
    OnClick() event enabled.
    The function has to be assigned to AlternateTurn() after a turn basis is coded.

---
+++ Assets/Scripts/Turn-Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Media;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Turn-Script : MonoBehaviour
+{
+        Public bool switchTurn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (switchTurn == true)
+        {
+            playerGo();
+        } else
+        {
+            opponentGo();
+        }
+    }
+    Public void AlternateTurn()
+    {
+        switchTurn = !switchTurn;
+    }
+    void playerGo()
+    {
+
+    }
+    void opponentGo()
+    {
+
+    }
+
+}
