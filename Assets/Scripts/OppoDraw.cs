6dd56ae977e41843223a41c1737c4382b8c7a482
JAG0824 <jahan@uwm.edu>
2020-10-04 19:34:35 -0500

    Draw
    New Script/Prefab

---
+++ Assets/Scripts/OppoDraw.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OppoDraw : MonoBehaviour
+{
+    public GameObject Card;
+    public GameObject CardStack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CardStack = GameObject.Find("Opponent-Player");
+        Card.transform.SetParent(Opponent-Player.transform);
+        Card.transform.localScale = Vector3.one;
+        Card.transform.position = new Vector3(transform.position.x, transform.position.y, -48);
+        Card.transform.eulerAngles = new Vector3(25, 0, 0);
+
+    }
+}
---
+++ Assets/Scripts/PlayerDraw.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDraw : MonoBehaviour
+{
+    public GameObject Card;
+    public GameObject CardStack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CardStack = GameObject.Find("Hand-Player");
+        Card.transform.SetParent(Hand-Player.transform);
+        Card.transform.localScale = Vector3.one;
+        Card.transform.position = new Vector3(transform.position.x, transform.position.y, -48);
+        Card.transform.eulerAngles = new Vector3(25, 0, 0);
+    }
+}
