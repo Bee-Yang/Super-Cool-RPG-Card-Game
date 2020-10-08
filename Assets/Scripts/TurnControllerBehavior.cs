366410a6f1d13197309c9d89986f42a152995dc0
Bee-Yang <yang737@uwm.edu>
2020-10-08 13:42:43 -0500

    Combined player draw and opponent draw into 1 script
    -Combined player draw and opponent draw into 1 card and put it into DeckBehavior script
    -Added commented code for drawing a card into turn controller script

--- Assets/Scripts/TurnControllerBehavior.cs
+++ Assets/Scripts/TurnControllerBehavior.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnControllerBehavior : MonoBehaviour
 {
     private bool isPlayerTurn;
+    private DeckBehavior player, enemy;
+
+    public GameObject playerDeck, enemyDeck;
+
     public bool IsPlayerTurn {
         get { return isPlayerTurn; }
         set { isPlayerTurn = value; }
-}
+    }
+
+    void Awake()
+    {
+        player = playerDeck.GetComponent<DeckBehavior>();
+        enemy = enemyDeck.GetComponent<DeckBehavior>();
+    }
 
     void Start()
     {
@@ -32,10 +43,22 @@ public class TurnControllerBehavior : MonoBehaviour
     void playerGo()
     {
         //add calls for functions for draw card, and begin battle here. turn ended manually by button
+        /*
+        if (playerDeck.transform.childCount > 0)
+        {
+            player.Draw();
+        }
+        */
     }
     void opponentGo()
     {
         //phases will be controlled here, turn ended automatically
+        /*
+        if (enemyDeck.transform.childCount > 0)
+        {
+            enemy.Draw();
+        }
+        */
         AlternateTurn();
     }
 
