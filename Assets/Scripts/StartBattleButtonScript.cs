4eba50b4e13c5c982866703e0564edd067a43474
bloatedfish <kira.sai.takashi@gmail.com>
2020-10-08 12:27:15 -0500

    Modified StartBattleButton Script

---
+++ Assets/Scripts/StartBattleButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartBattleButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public Button startBattleButton;
+   public TurnControllerBehavior turnController;
+
+	void Start () {
+		Button btn = startBattleButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		if (turnController.IsPlayerTurn) {
+
+        }
+	}
+}
