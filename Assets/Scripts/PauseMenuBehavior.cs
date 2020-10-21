1e93dee966f7d4a1a3fa471b986ea022b7221828
cocomuffinzz <crollins@uwm.edu>
2020-10-20 23:21:31 -0500

    wrote script for pause menu behavior

---
+++ Assets/Scripts/PauseMenuBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuBehavior : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseMenu;
+    private TurnControllerBehavior turnController;
+
+    void Start () {
+        pauseMenu.SetActive(false);
+        turnController = GameObject.Find("TurnController").GetComponent<TurnControllerBehavior>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if(isPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    void Pause() {
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+
+    public void ForfeitGame(){
+        Debug.Log("Forfeiting game...");
+        turnController.SetPhase(-2);
+    }
+
+    public void QuitGame(){
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
